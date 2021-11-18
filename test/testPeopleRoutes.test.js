f9d3b35c6f40615c3016c8db18d5de1581c66405
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2021-11-19 06:58:42 +0700

    Add more tests on people routes

--- test/testPeopleRoutes.test.js
+++ test/testPeopleRoutes.test.js
@@ -40,6 +40,18 @@ async function axiosGet(endpoint) {
 }
 
 /**
+ * PATH request to specific endpoint
+ * @param {string} endpoint 
+ * @returns Promise of http response
+ */
+ async function axiosPatch(endpoint) {
+    // create a promise for the axios request
+    const promise = axios.patch(endpoint);
+    const dataPromise = promise.then((response) => response);
+    return dataPromise;
+}
+
+/**
  * Test Suite No.1
  * Test status code of each path
  */
@@ -117,8 +129,210 @@ describe("Test status codes" , () => {
         })
     });
 
-     /**
+    /**
      * Test Case ID: 6
+     * Test GET people by_reservationID:reservationId response status code
+     * when that reservationId has values stored in database
+     * Expected Value : 200 - OK
+     */
+    test("Test status code GET people by reservationId", () => {
+        const reservationId = "1";
+        const endpoint = URL + "/by_reservationID/" + reservationId;
+        return axiosGet(endpoint)
+        .then(data => {
+            expect(data.status).toEqual(200);
+        })
+    });
+
+    /**
+     * Test Case ID: 7
+     * Test GET people by_reservationID:reservationId response status code
+     * when that reservationId has no values stored in database
+     * Expected Value : 204 - No Content
+     */
+    test("Test status code GET people by reservationId  (no value)", () => {
+        const reservationId = "10000";
+        const endpoint = URL + "/by_reservationID/" + reservationId;
+        return axiosGet(endpoint)
+        .then(data => {
+            expect(data.status).toEqual(204);
+        })
+    });
+
+    /**
+     * Test Case ID: 8
+     * Test GET people by_reservationID:reservationId response status code
+     * when that reservationId is invalid format
+     * Expected Value : 400 - Bad Input
+     */
+    test("Test status code GET people by reservationId", () => {
+        const reservationId = "asd";
+        const endpoint = URL + "/by_reservationID/" + reservationId;
+        return axiosGet(endpoint)
+        .catch(data => {
+            expect(data.response.status).toEqual(400);
+        })
+    });
+
+    /**
+     * Test Case ID: 9
+     * Test GET people by_reservationID:reservationId response status code
+     * when that reservationId is empty
+     * Expected Value : 406 - No reservationId included in request
+     */
+    test("Test status code GET people by reservationId", () => {
+        const reservationId = "abcde";
+        const endpoint = URL + "/by_reservationID/" + reservationId;
+        return axiosGet(endpoint)
+        .catch(data => {
+            expect(data.response.status).toEqual(406);
+        })
+    });
+
+    /**
+     * Test Case ID: 10
+     * Test GET people by_reservationID:reservationId response status code
+     * when that date has values stored in database
+     * Expected Value : 200 - OK
+     */
+         test("Test status code GET people by reservationId", () => {
+            const reservationId = "10000";
+            const endpoint = URL + "/by_reservationID/" + reservationId;
+            return axiosGet(endpoint)
+            .then(data => {
+                expect(data.status).toEqual(204);
+            })
+        });
+
+    /**
+     * Test Case ID: 11
+     * Test status code of '/count/total/:date'
+     * when that date has values stored in database
+     * Expected value : 200 - OK
+     */
+     test("Test status code GET count people by date", () => {
+        const date = "11-11-2021";
+        const endpoint = URL + "/count/total/" + date;
+        return axiosGet(endpoint)
+        .then(res => {
+            expect(res.status).toEqual(200);
+        })
+    });
+
+    /**
+     * Test Case ID: 12
+     * Test status code of '/count/total/:date'
+     * when that date has no values stored in database
+     * Expected value : 400 - Bad Input
+     */
+    test("Test status code GET count people by date (invalid date param)", () => {
+        const date = "11-11-3000";
+        const endpoint = URL + "/count/total/" + date;
+        return axiosGet(endpoint)
+        .catch(res => {
+            let statusCode = res.response.status;
+            expect(statusCode).toEqual(400);
+        })
+    });
+
+    /**
+     * Test Case ID: 13
+     * Test status code of '/count/total/:date'
+     * when that date has values stored in database
+     * Expected value : 400 - Bad Input
+     */
+     test("Test status code GET count people by date (invalid date param)", () => {
+        const date = "11/11/2021";
+        const endpoint = URL + "/count/total/" + date;
+        return axiosGet(endpoint)
+        .catch(res => {
+            let statusCode = res.response.status;
+            expect(statusCode).toEqual(400);
+        })
+    });
+
+
+    /**
+     * Test Case ID: 14
+     * Test status code of '/count/total/:date'
+     * when the date param is empty
+     * Expected value : 406 - Bad Input
+     */
+     test("Test status code GET count people by date (invalid date param)", () => {
+        const date = "";
+        const endpoint = URL + "/count/total/" + date;
+        return axiosGet(endpoint)
+        .catch(res => {
+            let statusCode = res.response.status;
+            expect(statusCode).toEqual(406);
+        })
+    });
+
+    /**
+     * Test Case ID: 15
+     * Test status code of '/count/walkin/:date'
+     * when that date has values stored in database
+     * Expected value : 200 - OK
+     */
+    test("Test status code GET count people by date", () => {
+        const date = "11-11-2021";
+        const endpoint = URL + "/count/walkin/" + date;
+        return axiosGet(endpoint)
+        .then(res => {
+            expect(res.status).toEqual(200);
+        })
+    });
+
+    /**
+     * Test Case ID: 16
+     * Test status code of '/count/walkin/:date'
+     * when that date has no values stored in database
+     * Expected value : 400 - Bad Input
+     */
+    test("Test status code GET count people by date (invalid date param)", () => {
+        const date = "11-11-3000";
+        const endpoint = URL + "/count/walkin/" + date;
+        return axiosGet(endpoint)
+        .catch(res => {
+            let statusCode = res.response.status;
+            expect(statusCode).toEqual(400);
+        })
+    });
+
+    /**
+     * Test Case ID: 17
+     * Test status code of '/count/walkin/:date'
+     * when that date has values stored in database
+     * Expected value : 400 - Bad Input
+     */
+        test("Test status code GET count people by date (invalid date param)", () => {
+        const date = "11/11/2021";
+        const endpoint = URL + "/count/walkin/" + date;
+        return axiosGet(endpoint)
+        .catch(res => {
+            let statusCode = res.response.status;
+            expect(statusCode).toEqual(400);
+        })
+    });
+
+    /**
+     * Test Case ID: 18
+     * Test status code of '/count/walkin/:date'
+     * when the date param is empty
+     * Expected value : 406 - Bad Input
+     */
+        test("Test status code GET count people by date (invalid date param)", () => {
+        const date = "";
+        const endpoint = URL + "/count/walkin/" + date;
+        return axiosGet(endpoint)
+        .catch(res => {
+            let statusCode = res.response.status;
+            expect(statusCode).toEqual(406);
+        })
+    });
+
+    /**
+     * Test Case ID: 19
      * Test status code of POST method when POST a data to database
      * Expected value : 200 - OK
      */
@@ -126,13 +340,13 @@ describe("Test status codes" , () => {
         const date = "11-11-2030";
         const endpoint = baseURL + "/getDataFromGov/" + date;
         return axiosPost(endpoint)
-        .catch(res => {
-            expect(res.response.status).toEqual(200);
+        .then(res => {
+            expect(res.status).toEqual(200);
         })
     });
 
     /**
-     * Test Case ID: 7
+     * Test Case ID: 20
      * Test status code of POST method when POST duplicate data to database
      * Expected value : 401
      */
@@ -146,7 +360,7 @@ describe("Test status codes" , () => {
     });
 
     /**
-     * Test Case ID: 8
+     * Test Case ID: 21
      * Test status code of DELETE method when DELETE a date from database
      * Expected value : 200 - OK
      */
@@ -155,8 +369,21 @@ describe("Test status codes" , () => {
         const endpoint = URL + "/by_date/" + date;
         return axiosDelete(endpoint)
         .then(res => {
-            console.log(res);
             expect(res.status).toEqual(200);
+        })
+    })
+
+    /**
+     * Test Case ID: 22
+     * Test status code of DELETE method when DELETE a date from database
+     * Expected value : 200 - OK
+     */
+    test("Test status code DELETE duplicate people by date", () => {
+        const date = "11-11-2030";
+        const endpoint = URL + "/by_date/" + date;
+        return axiosDelete(endpoint)
+        .catch(res => {
+            expect(res.response.status).toEqual(400);
         })
     })
 });
@@ -167,7 +394,7 @@ describe("Test status codes" , () => {
  */
 describe("Test response data structure" , () => {
     /**
-     * Test Case ID: 9
+     * Test Case ID: 1
      * Test structure of '/by_date/:date' response
      * when that date have values stored in database
      */
@@ -180,7 +407,7 @@ describe("Test response data structure" , () => {
     });
 
     /**
-     * Test Case ID: 10
+     * Test Case ID: 2
      * Test structure of '/by_date/:date' response
      * when that date hav values stored in database
      */
@@ -197,7 +424,7 @@ describe("Test response data structure" , () => {
     });
 
     /**
-     * Test Case ID: 11
+     * Test Case ID: 3
      * Test structure of '/by_date/:date' response
      * when that date has no value stored in database
      */
@@ -214,7 +441,7 @@ describe("Test response data structure" , () => {
     });
 
     /**
-     * Test Case ID: 12
+     * Test Case ID: 4
      * Test structure of a person from '/by_date/:date' response
      */
     test("Test GET a person in people by date structure", () => {
@@ -235,17 +462,62 @@ describe("Test response data structure" , () => {
     });
 
     /**
-     * Test Case ID: 13
+     * Test Case ID: 5
+     * Test POST response message when post people to database
+     * Expected value : "data added"
+     */
+    test("Test POST response message when post people to database", () => {
+        const date = "11-11-2030";
+        const endpoint = baseURL + "/getDataFromGov/" + date;
+        return axiosPost(endpoint)
+        .then(res => {
+            const data = res.data;
+            expect(data.msg).toEqual("data added");
+        });
+    });
+
+    /**
+     * Test Case ID: 6
      * Test POST response message when post duplicate people to database
      * Expected value : "already have data in this date."
      */
     test("Test POST response message when post duplicate people to database", () => {
-        const date = "11-11-2021";
+        const date = "11-11-2030";
         const endpoint = baseURL + "/getDataFromGov/" + date;
         return axiosPost(endpoint)
         .catch(res => {
             const data = res.response.data;
             expect(data.msg).toEqual("already have data in this date.");
+        });
+    });
+
+    /**
+     * Test Case ID: 7
+     * Test POST response message when delete people from database
+     * Expected value : "Deleted vaccine reservations from ${date}."
+     */
+    test("Test DELETE response message when delete people from database", () => {
+        const date = "11-11-2030";
+        const endpoint = URL + "/by_date/" + date;
+        return axiosDelete(endpoint)
+        .then(res => {
+            const data = res.data;
+            expect(data.msg).toEqual(`Deleted vaccine reservations from ${date}.`);
+        });
+    });
+
+    /**
+     * Test Case ID: 8
+     * Test POST response message when delete people from database
+     * Expected value : "Can't find the date in the database""
+     */
+    test("Test DELETE response message when duplicate delete people from database", () => {
+        const date = "11-11-2030";
+        const endpoint = URL + "/by_date/" + date;
+        return axiosDelete(endpoint)
+        .catch(res => {
+            const data = res.response.data;
+            expect(data.msg).toEqual("Can't find the date in the database");
         });
     });
 });
